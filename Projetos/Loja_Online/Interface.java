7ce3abc2d39ba970a7896ebc21f1b605a86cc1b8
wev_digital <Wevertonmartinsdigital@gmail.com>
2025-02-25 23:26:50 -0300

    Criação de uma interface gráfica para a loja online com uma janela principal, contendo quatro painéis: menu, busca, produtos e opções, organizados verticalmente.

--- Projetos/Loja_Online/Interface.java
+++ Projetos/Loja_Online/Interface.java
@@ -1,115 +1,79 @@
 package Projetos.Loja_Online;
 
-import javax.swing.*; 
-import java.awt.*;
-
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 public class Interface {
     public static void main(String[] args) {
-        // Criação da janela principal do aplicativo
+        // Criação do JFrame
         JFrame janelaFrame = new JFrame("LOJA ONLINE");
         janelaFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        janelaFrame.setSize(new Dimension(420, 720));
-        janelaFrame.setMinimumSize(new Dimension(375, 667));
+        janelaFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        janelaFrame.setMinimumSize(new Dimension(420, 720));
+        janelaFrame.setLayout(new BorderLayout());
 
-        // Criação do painel principal da loja
-        JPanel homePanel = createHomePanel();
-        janelaFrame.add(homePanel);
+        // Criação do painel principal com BoxLayout vertical
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.setBackground(new Color(255,240,245));
 
-        janelaFrame.setVisible(true);
+        // Criação do primeiro painel
+        JPanel menuPanel = createMenuPanel();
+        mainPanel.add(menuPanel);
+
+        // Criação do segundo painel
+        JPanel searchPanel = createSearchPanel();
+        mainPanel.add(searchPanel);
+
+        // Criação do terceiro painel
+        JPanel productsPanel = createProductsPanel();
+        mainPanel.add(productsPanel);
+
+        // Criação do quarto painel
+        JPanel optionsPanel = createOptionsPanel();
+        mainPanel.add(optionsPanel);
+
+        // Adiciona o painel principal ao JFrame
+        janelaFrame.add(mainPanel);
+        janelaFrame.setVisible(true); // Torna a janela visível
     }
 
-    // Método para criar o painel principal da loja
-    public static JPanel createHomePanel() {
-        JPanel homePanel = new JPanel();
-        homePanel.setLayout(new BoxLayout(homePanel, BoxLayout.Y_AXIS));
-        homePanel.setBackground(new Color(224, 255, 255));
+    // Primeiro painel
+    private static JPanel createMenuPanel() {
+        JPanel menuPanel = new JPanel();
+        menuPanel.setBackground(new Color(255,240,245));
+        menuPanel.setAlignmentX(JPanel.CENTER_ALIGNMENT); // Centraliza o painel horizontalmente
 
-        // Criação do painel superior
-        JPanel topPanel = createTopPanel();
-        homePanel.add(topPanel);
-
-        // Criação do painel central
-        JPanel centerPanel = createcenterPanel();
-        homePanel.add(centerPanel);
-        
-
-        return homePanel; // Retorna o painel principal
+        return menuPanel; // Retorna o painel
     }
 
-    // Método para criar o painel superior
-    private static JPanel createTopPanel() {
-        JPanel topPanel = new JPanel();
-        topPanel.setBackground(new Color(224, 255, 255));
+    // Segundo painel
+    private static JPanel createSearchPanel() {
+        JPanel searchPanel = new JPanel();
+        searchPanel.setBackground(new Color(255,240,245));
+        searchPanel.setAlignmentX(JPanel.CENTER_ALIGNMENT); // Centraliza o painel horizontalmente
 
-        JButton menuButton = createImageButton("Projetos/Loja_Online/img/menu.blue.png", 40, 40);
-        menuButton.setPreferredSize(new Dimension(40, 40));
-        menuButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
-        menuButton.setContentAreaFilled(false);
-        menuButton.setFocusPainted(false);
-        menuButton.setBorderPainted(false);
-
-        JLabel titleLabel = new JLabel("Loja Online");
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 30));
-        titleLabel.setForeground(new Color(0,191,255));
-        titleLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-
-        JButton shopButton = createImageButton("Projetos/Loja_Online/img/shop.blue.png", 40, 40);
-        shopButton.setPreferredSize(new Dimension(40, 40));
-        shopButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
-        shopButton.setContentAreaFilled(false);
-        shopButton.setFocusPainted(false);
-        shopButton.setBorderPainted(false);
-
-        // Adição dos componentes ao painel
-        topPanel.add(Box.createVerticalStrut(10));
-        topPanel.add(menuButton);
-        topPanel.add(Box.createHorizontalStrut(30));
-        topPanel.add(titleLabel);
-        topPanel.add(Box.createHorizontalStrut(30));
-        topPanel.add(shopButton);
-
-        return topPanel; // Retorna o painel superior
+        return searchPanel; // Retorna o painel
     }
 
-    // Método para criar o painel central
-    private static JPanel createcenterPanel() {
-        JPanel centerPanel = new JPanel();
-        centerPanel.setBackground(new Color(224, 255, 255));
-        centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.X_AXIS)); 
+    // Terceiro painel
+    private static JPanel createProductsPanel() {
+        JPanel productsPanel = new JPanel();
+        productsPanel.setBackground(new Color(255,240,245));
+        productsPanel.setAlignmentX(JPanel.CENTER_ALIGNMENT); // Centraliza o painel horizontalmente
 
-        JTextField searchField = new JTextField("");
-        searchField.setMaximumSize(new Dimension(400, 50));
-        searchField.setAlignmentX(JTextField.CENTER_ALIGNMENT);
-
-        JButton productsButton = createImageButton("Projetos/Loja_Online/img/sacola_blue.png", 60, 50);
-        productsButton.setContentAreaFilled(false);
-        productsButton.setFocusPainted(false);
-        productsButton.setBorderPainted(false);
-
-        // Adição dos componentes ao painel
-        centerPanel.add(Box.createHorizontalStrut(30));
-        centerPanel.add(searchField);
-        centerPanel.add(Box.createHorizontalStrut(20));
-        centerPanel.add(productsButton);
-
-        return centerPanel; // Retorna o painel central
+        return productsPanel; // Retorna o painel
     }
 
-    // Método para criar um botão com imagem redimensionada
-    private static JButton createImageButton(String imagePath, int width, int height) {
-        // Carregar a imagem
-        ImageIcon icon = new ImageIcon(imagePath);
-            
-        // Redimensionar a imagem para caber no botão
-        Image image = icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
-            
-        // Criar um novo ImageIcon com a imagem redimensionada
-        JButton button = new JButton(new ImageIcon(image));
-        button.setPreferredSize(new Dimension(width, height)); // Define o tamanho preferido do botão
-        button.setContentAreaFilled(false);
-        button.setBorderPainted(false);
-        button.setFocusPainted(false);
-    
-        return button; // Retorna o botão
+    // Quarto painel
+    private static JPanel createOptionsPanel() {
+        JPanel optionsPanel = new JPanel();
+        optionsPanel.setBackground(new Color(255,240,245));
+        optionsPanel.setAlignmentX(JPanel.CENTER_ALIGNMENT); // Centraliza o painel horizontalmente
+
+        return optionsPanel; // Retorna o painel
     }
 }
--- README.md
+++ README.md
@@ -2,14 +2,4 @@
 Este repositório foi desenvolvido com o propósito de armazenar e compartilhar uma coleção de projetos implementados na linguagem de programação Java. Cada projeto aqui contido foi criado com a intenção de demonstrar diferentes aspectos e funcionalidades da linguagem, bem como suas aplicações práticas em diversas áreas.
 
 # DIA 001 // PROJETO --- LOJA ONLINE
-Melhorias na Interface Gráfica da Loja Online
-
-Layout do Painel Principal: O homePanel agora utiliza BoxLayout com orientação vertical (BoxLayout.Y_AXIS), proporcionando uma disposição mais organizada e responsiva dos componentes.
-
-Espaçamento no Painel Superior: Adicionado um espaçamento vertical (Box.createVerticalStrut(10)) antes do botão de menu no topPanel, melhorando a estética e a separação visual dos elementos.
-
-Layout do Painel Central: O centerPanel foi atualizado para usar BoxLayout com orientação horizontal (BoxLayout.X_AXIS), permitindo que os componentes sejam alinhados lado a lado de forma mais eficiente.
-
-Ajuste no Tamanho do Botão de Produtos: O tamanho do botão de produtos foi ajustado para 60x50 pixels, melhorando a usabilidade e a aparência.
-
-Essas alterações visam aprimorar a experiência do usuário, tornando a interface mais intuitiva e visualmente agradável.
+Criação de uma interface gráfica para a loja online com uma janela principal, contendo quatro painéis: menu, busca, produtos e opções, organizados verticalmente.
